522a7d0510f47ea20e8d2629b5c0e1637e0a9e05
warrenronsiek <warrenronsiek@gmail.com>
2019-07-18 15:24:53 -0700

    still working on it...

--- enable_star_imports.py
+++ enable_star_imports.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import sys
+from difflib import Differ
+from pprint import pprint
 
 transformer_string = """
 from clarify.enable_star_imports import CodeFactory
@@ -16,11 +18,12 @@ class Code(CodeFactory):
 
 
 def enable_star_imports(path):
-    print(f"running on {path}")
     all_objects_in_path = os.listdir(path)
     non_special_objects = [d for d in all_objects_in_path if not d.startswith(('_', '.'))]
     folders = [f for f in non_special_objects if '.' not in f and len(os.listdir(os.path.join(path, f))) != 0]
+    folders.sort()
     python_files = [f.replace('.py', '') for f in non_special_objects if f.endswith('.py')]
+    python_files.sort()
 
     init_file_name = os.path.join(path, '__init__.py')
     init_contents = f"""
@@ -38,23 +41,25 @@ for item in __all__:
     path_contains_transformer = len([file for file in files if file.endswith(transformer_file_indicators)]) > 0
     path_contains_folder = len(folders) > 0
 
-
+    differ = Differ()
     with open(init_file_name, 'r') as read_file:
+
+        def compare_overwrite(write_contents):
+            current_text = read_file.read()
+            if current_text != write_contents:
+                print(f"writing to {init_file_name}")
+                pprint(list(differ.compare(current_text.splitlines(keepends=True),
+                                           write_contents.splitlines(keepends=True))))
+                with open(init_file_name, 'w+') as write_file:
+                    write_file.write(write_contents)
+
         if path_contains_transformer and not path_contains_folder:
-            if read_file.read() != transformer_string:
-                print(f"writing Code to {init_file_name}")
-                with open(init_file_name, 'w+') as write_file:
-                    write_file.write(transformer_string)
+            compare_overwrite(transformer_string)
         elif path_contains_transformer and path_contains_folder:
-            if read_file.read() != transformer_string + init_contents:
-                print(f"writing Code and __all__ to {init_file_name}")
-                with open(init_file_name, 'w+') as write_file:
-                    write_file.write(transformer_string + init_contents)
+            compare_overwrite(transformer_string + init_contents)
         elif non_special_objects:
-            if read_file.read() != init_contents:
-                print(f"writing __all__ to {init_file_name}")
-                with open(init_file_name, 'w+') as write_file:
-                    write_file.write(init_contents)
+            compare_overwrite(init_contents)
+
     for folder in folders:
         enable_star_imports(os.path.join(path, folder))
 
