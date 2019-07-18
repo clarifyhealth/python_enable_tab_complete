d0fb7df30f29910f3891da3eeb98c91411092430
warrenronsiek <warrenronsiek@gmail.com>
2019-07-18 13:25:15 -0700

    still working on it...

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='python_enable_tab_complete',
     version='1.0.0',
-    py_modules=['enable_star_imports.py']
+    py_modules=['enable_star_imports']
 )
