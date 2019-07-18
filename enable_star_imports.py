ab86018c43fc16ecb1388b20874f0789c17336c0
warrenronsiek <warrenronsiek@gmail.com>
2019-07-18 14:49:55 -0700

    still working on it...

--- enable_star_imports.py
+++ enable_star_imports.py
@@ -16,6 +16,7 @@ class Code(CodeFactory):
 
 
 def enable_star_imports(path):
+    print(f"running on {path}")
     all_objects_in_path = os.listdir(path)
     non_special_objects = [d for d in all_objects_in_path if not d.startswith(('_', '.'))]
     folders = [f for f in non_special_objects if '.' not in f and len(os.listdir(os.path.join(path, f))) != 0]
@@ -37,17 +38,23 @@ for item in __all__:
     path_contains_transformer = len([file for file in files if file.endswith(transformer_file_indicators)]) > 0
     path_contains_folder = len(folders) > 0
 
-    with open(init_file_name, 'w+') as file:
+
+    with open(init_file_name, 'r') as read_file:
         if path_contains_transformer and not path_contains_folder:
-            logging.info(f"writing Code to {init_file_name}")
-            file.write(transformer_string)
+            if read_file.read() != transformer_string:
+                print(f"writing Code to {init_file_name}")
+                with open(init_file_name, 'w+') as write_file:
+                    write_file.write(transformer_string)
         elif path_contains_transformer and path_contains_folder:
-            logging.info(f"writing Code and __all__ to {init_file_name}")
-            file.write(transformer_string)
-            file.write(init_contents)
+            if read_file.read() != transformer_string + init_contents:
+                print(f"writing Code and __all__ to {init_file_name}")
+                with open(init_file_name, 'w+') as write_file:
+                    write_file.write(transformer_string + init_contents)
         elif non_special_objects:
-            logging.info(f"writing __all__ to {init_file_name}")
-            file.write(init_contents)
+            if read_file.read() != init_contents:
+                print(f"writing __all__ to {init_file_name}")
+                with open(init_file_name, 'w+') as write_file:
+                    write_file.write(init_contents)
     for folder in folders:
         enable_star_imports(os.path.join(path, folder))
 
