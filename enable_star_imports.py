ad5d47a68891b12af5f40d7995bee47df6328161
warrenronsiek <warrenronsiek@gmail.com>
2019-07-29 14:26:01 -0700

    now compatible with tera1680

--- enable_star_imports.py
+++ enable_star_imports.py
@@ -3,18 +3,7 @@ import sys
 from difflib import Differ
 from pprint import pprint
 from shutil import rmtree
-
-transformer_string = """
-from clarify.enable_star_imports import CodeFactory
-from clarify.library.progress_logger import ProgressLogger
-import os
-
-class Code(CodeFactory):
-    def __init__(self, parameters, progress_logger: ProgressLogger = None, verify_count_remains_same=False):
-        self.location = os.path.dirname(os.path.abspath(__file__))
-        super().__init__(parameters, progress_logger, verify_count_remains_same)
-
-"""
+import re
 
 
 def delete_empty_dirs(path):
@@ -46,53 +35,38 @@ def enable_star_imports(path):
     all_objects_in_path = os.listdir(path)
     non_special_objects = [d for d in all_objects_in_path if not d.startswith(('_', '.')) and not d.startswith('drgpy')]
     folders = [f for f in non_special_objects if '.' not in f and len(os.listdir(os.path.join(path, f))) != 0]
-    folders.sort()
-    python_files = [f.replace('.py', '') for f in non_special_objects if f.endswith('.py')]
-    python_files.sort()
-
-    init_file_name = os.path.join(path, '__init__.py')
-    init_contents = f"""
-__all__ = {folders + python_files}
-for item in __all__:
-    try:
-        exec("from . import " + item)
-    except ImportError:
-        print("WARNING - trying to import non existent module" + item)
-
-"""
-
     files = [f for f in all_objects_in_path if '.' in f and not f.startswith('_')]
     transformer_file_indicators = ('.sql', '.json', '.csv', 'calculate.py')
     path_contains_transformer = len([file for file in files if file.endswith(transformer_file_indicators)]) > 0
-    path_contains_folder = len(folders) > 0
 
-    differ = Differ()
-    try:
-        with open(init_file_name, 'r') as read_file:
+    def write_transformer(file_name):
+        transformer_reader_class_name = ''.join([s.title() for s in file_name.split('_')])
+        transformer_reader_string = f"""
+from clarify.enable_star_imports import CodeFactory
+from clarify.library.progress_logger import ProgressLogger
+import os
 
-            def compare_overwrite(write_contents):
-                current_text = read_file.read()
-                if current_text != write_contents:
-                    print(f"writing to {init_file_name}")
-                    pprint(list(differ.compare(current_text.splitlines(keepends=True),
-                                               write_contents.splitlines(keepends=True))))
-                    with open(init_file_name, 'w+') as write_file:
-                        write_file.write(write_contents)
 
-            if path_contains_transformer and not path_contains_folder:
-                compare_overwrite(transformer_string)
-            elif path_contains_transformer and path_contains_folder:
-                compare_overwrite(transformer_string + init_contents)
-            elif non_special_objects:
-                compare_overwrite(init_contents)
-    except FileNotFoundError:
-        with open(init_file_name, 'w+') as write_file:
-            if path_contains_transformer and not path_contains_folder:
-                write_file.write(transformer_string)
-            elif path_contains_transformer and path_contains_folder:
-                write_file.write(transformer_string + init_contents)
-            elif non_special_objects:
-                write_file.write(init_contents)
+class {transformer_reader_class_name}(CodeFactory):
+    def __init__(self, parameters, progress_logger: ProgressLogger = None, verify_count_remains_same=False):
+        self.location = os.path.dirname(os.path.abspath(__file__))
+        super().__init__(parameters, progress_logger, verify_count_remains_same)
+"""
+        if path_contains_transformer:
+            with open(os.path.join(path, file_name + '.py'), 'w+') as file:
+                file.write(transformer_reader_string)
+
+    if 'features/' in path or 'data_sources/' in path or 'ccgs/' in path:
+        if 'features/' in path:
+            transformer_reader_file_name = path[re.search(r'/clarify/library/features/', path).end():].replace('/', '_')
+            write_transformer(transformer_reader_file_name)
+        elif 'ccgs/' in path:
+            transformer_reader_file_name = path[re.search(r'/clarify/library/ccgs/', path).end():].replace('/', '_')
+            write_transformer(transformer_reader_file_name)
+        elif re.search(r'/clarify/library/data_sources/[a-z_0-9]+/[a-z_0-9]+/', path):
+            transformer_reader_file_name = path[re.search(r'/clarify/library/data_sources/[a-z_]+/', path).end():]\
+                .replace('/', '_')
+            write_transformer(transformer_reader_file_name)
 
     for folder in folders:
         enable_star_imports(os.path.join(path, folder))
