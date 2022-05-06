03704c8330609a51a1977971177a8b05ea5998da
Ankush Kankariya <ankush@clarifyhealth.com>
2022-05-06 14:09:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_enable_tab_complete',
-    version='1.1.0',
+    version='1.1.1',
     py_modules=['enable_star_imports'],
     entry_points={
         'console_scripts': ['enable_star_imports = enable_star_imports:main']
